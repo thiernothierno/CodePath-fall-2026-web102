626366121d3f75081a609d5afa7f5f77af1d9643
Thierno Barry <thrndicko@gmail.com>
2026-09-18 14:48:31 -0600

    initial commit

---
+++ OneDrive/Desktop/Web102-Fall-2026/Samosa-Selector/samosa-selector/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Samosa Selector</h1><br /><br />
+        <h2>Count:</h2>
+        <img className="samosa" src="/samosa-image.png" alt="samosa" />
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+
